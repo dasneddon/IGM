b5d462eb48b0312607b3fa4161ea1251e11ffa11
David Sneddon <dasneddon@outlook.com>
2024-12-01 00:15:08 -0500

    modified:   .gitignore deleted:    10.1007%2Fs11113-020-09608-5-citation.ris deleted:    GDIM_Description_2023_03.pdf deleted:    IEM.docx deleted:    IEM.pdf deleted:    IEM.qmd renamed:    iem.R -> IGM.R deleted:    IGM.docx modified:   IGM.pdf modified:   IGM.qmd deleted:    Proposal.html deleted:    Proposal.pdf deleted:    Proposal.qmd deleted:    Proposal_files/libs/bootstrap/bootstrap-icons.css deleted:    Proposal_files/libs/bootstrap/bootstrap-icons.woff deleted:    Proposal_files/libs/bootstrap/bootstrap.min.css deleted:    Proposal_files/libs/bootstrap/bootstrap.min.js deleted:    Proposal_files/libs/clipboard/clipboard.min.js deleted:    Proposal_files/libs/quarto-html/anchor.min.js deleted:    Proposal_files/libs/quarto-html/popper.min.js deleted:    Proposal_files/libs/quarto-html/quarto-syntax-highlighting.css deleted:    Proposal_files/libs/quarto-html/quarto.js deleted:    Proposal_files/libs/quarto-html/tippy.css deleted:    Proposal_files/libs/quarto-html/tippy.umd.min.js deleted:    Table_7_percentile_dollar_crosswalk.csv deleted:    The changing geography of social mobility in the United States.pdf modified:   bibliography.bib deleted:    censusdata.R deleted:    citation-319182204.bib deleted:    connor-storper-2020-the-changing-geography-of-social-mobility-in-the-united-states.pdf modified:   county_rank_slope.csv deleted:    county_rank_slope.xlsx deleted:    csa-counties.xlsx deleted:    cz00_eqv_v1.xls deleted:    dec2020vars.csv deleted:    dp134208.pdf deleted:    example.qmd deleted:    geogs.csv deleted:    hendren-Where_is-Bibtex.bib deleted:    list1_2023.xlsx deleted:    list2_2023.xlsx deleted:    mobility_geo.pdf deleted:    online_data_tables-2.xls deleted:    online_data_tables.xls deleted:    online_table2.xlsx deleted:    online_table4.xlsx deleted:    online_table5.xls deleted:    principal.csv deleted:    principal_cities.xlsx deleted:    proposal2.qmd deleted:    s11113-020-09608-5.pdf deleted:    sampleimage.png deleted:    the-future-of-work-after-covid-19-report-vf.pdf deleted:    ~$IGM.docx

---
+++ IGM.R
@@ -0,0 +1,77 @@
+library(tigris)
+options(tigris_use_cache = TRUE)
+library(sf)
+library(dplyr)
+library(censusapi)
+library(units)
+library(geosphere)
+library(fixest)
+options(scipen=999)
+library(readr)
+
+
+
+###IMPORT GEOGRAPHIC DATA
+
+counties <- counties()
+counties$centroid <- st_centroid(counties$geometry)
+counties$lon <- sf::st_coordinates(counties$centroid)[,1]
+counties$lat <- sf::st_coordinates(counties$centroid)[,2]
+counties <- counties[,c("GEOID", "lon", "lat")]
+cbsa <- read.csv("csa-counties.csv")
+cbsa$GEOID <- paste0(formatC(cbsa$FIPS.State.Code, width=2, flag=0),
+                     formatC(cbsa$FIPS.County.Code, width=3, flag=0))
+
+###IMPORT COUNTY POPUATIONS
+
+county_pop <- getCensus(
+  "dec/dhc",
+  vintage = 2020,
+  vars = c("NAME", "P1_001N"),
+  region = "county:*")
+
+county_pop$GEOID <- paste0(county_pop$state, county_pop$county)
+county_pop <- merge(counties,county_pop, by="GEOID")
+county_pop$area <- set_units(st_area(county_pop$geometry), "mi^2")
+county_pop$density <- county_pop$P1_001N/county_pop$area
+
+##MERGE POPULATIONS AND GEOGRAPHY
+cbsa <- merge(cbsa,county_pop, by="GEOID")
+cbsa$densest <- 0
+cbsa$distance <- set_units(0, "m")
+
+##FIND THE DENSEST COUNTY
+
+densecounty <- function(combstatarea){
+  cbsaframe <- cbsa[cbsa$CBSA.Code==combstatarea,]
+  highest <- max(cbsaframe$density)
+  return(cbsaframe$GEOID[cbsaframe$density==highest])
+}
+
+for (i in unique(cbsa$CBSA.Code)){
+  flag <- densecounty(i)
+  cbsa$densest[cbsa$CBSA.Code==i] <- flag
+}
+
+for (i in 1:nrow(cbsa)){
+  flag <- cbsa$densest[i]  
+  cbsa$distance[i] <- distHaversine(c(cbsa[i,"lon"],
+                                      cbsa[i,"lat"]),
+                                    c(cbsa$lon[cbsa$GEOID==flag],
+                                      cbsa$lat[cbsa$GEOID==flag]))
+}
+
+#IMPORT RANK-RANK SLOPE
+
+county_rank_slope <- read_csv("county_rank_slope.csv")
+cbsa <- merge(cbsa, county_rank_slope, by="GEOID")
+cbsa$RS <- cbsa$RS*100
+
+##REGRESSION
+
+reg1 <- feols(RS ~ asinh(distance) | State.Name, 
+              data = cbsa, vcov = "hetero")
+
+summary(reg1)
+
+
